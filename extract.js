34a91aca6d491bd27e6f98f4f11780bfbd8863e4
jp-agustin <agustin.johnpatrick@gmail.com>
2018-06-04 13:31:55 +1000

    Task 2.1 - Passing

--- extract.js
+++ extract.js
@@ -1,5 +1,20 @@
 module.exports.extract = function(window) {
-  
-  // Write your solution to Task #2 - Extract Metadata task here
 
+  // Write your solution to Task #2 - Extract Metadata task here
+  // Parsing of form fields from the webpage
+  var hash = {};
+  var form = window.document.getElementsByTagName("form");
+  var rows = form[0].getElementsByTagName("tr");
+
+  for (let row of rows) {
+    // Excluding rows without control fields
+    if (row.cells.length === 2) {
+      let key = row.cells[0].textContent.trim();
+      let value = row.cells[1].firstChild.name;
+
+      hash[key] = key + " " + value;
+    }
+  }
+
+  return hash;
 }
