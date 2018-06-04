dbd8fa26ae20ce8bc05075ad6b87f85b90c15b09
jp-agustin <agustin.johnpatrick@gmail.com>
2018-06-04 13:52:46 +1000

    Task 2.2 - Passing

--- match.js
+++ match.js
@@ -1,5 +1,16 @@
 module.exports.match = function(hash) {
 
   // Write your solution to Task #2 - Match Metadata task here
-  
+  var output = [];
+  // Common expression for credit card date fields
+  var exp = "Ecom_Payment_Card_ExpDate";
+
+  for (let key in hash) {
+    // Searching exp in every hash values
+    if (hash[key].search(exp) !== -1) {
+      output.push(key);
+    }
+  }
+
+  return output;
 }
